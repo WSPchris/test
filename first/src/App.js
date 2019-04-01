52d285f933b9409c826124475d871978041cb9c0
WSPchris <1423456713@qq.com>
2019-04-02 01:25:51 +0800

    第二次修改

--- first/src/App.js
+++ first/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            第二次修改
           </a>
         </header>
       </div>
